88989de65750cffc674292d0e6d8637182c970ba
leonardo.goncalves <leogoncalves.contato@gmail.com>
2023-02-22 16:10:05 -0300

    💄 Update Index components

--- packages/shared/ui/src/components/index.ts
+++ packages/shared/ui/src/components/index.ts
@@ -1,2 +1,5 @@
+export { Grid, Col } from './Grid';
+export { default as Input } from './Input';
 export { default as Button } from './Button';
+export { default as Wrapper } from './Wrapper';
 export { default as Form, FormControl, FormGroup, Handle, useControlContext } from './Form';
