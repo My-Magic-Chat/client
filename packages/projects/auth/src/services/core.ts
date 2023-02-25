474a121d139eef011181f0411eee33018a08aa4a
leonardo.goncalves <leogoncalves.contato@gmail.com>
2023-02-25 11:59:22 -0300

    ✨ add core auth service

--- packages/projects/auth/src/services/core.ts
+++ packages/projects/auth/src/services/core.ts
@@ -1,3 +1,17 @@
-class Core { }
+import { Environment } from '@client/setup';
+
+class Core {
+    public environment = Environment;
+
+    constructor() {
+        this.setEnv();
+    }
+
+    private setEnv() {
+        new Environment({
+            env: process.env.REACT_APP_ENV
+        });
+    }
+}
 
 export default new Core();
--- packages/projects/auth/types/custom.d.ts
+++ packages/projects/auth/types/custom.d.ts
@@ -7,6 +7,6 @@ declare module "*.svg" {
 
 declare namespace NodeJS {
     export interface ProcessEnv {
-        REACT_APP_API_URL: string;
+        REACT_APP_ENV: string;
     }
 }
