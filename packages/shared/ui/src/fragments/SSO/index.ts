f2c3fb55bb13501f33b9d495316bf9807dfc7965
leonardo.goncalves <leogoncalves.contato@gmail.com>
2023-02-25 00:18:58 -0300

    🏷️ export forgot types

--- packages/shared/ui/src/fragments/SSO/index.ts
+++ packages/shared/ui/src/fragments/SSO/index.ts
@@ -1,4 +1,5 @@
 export { Signin } from './Signin';
 export { Signup } from './Signup';
+export { Forgot } from './Forgot';
 
 export * from './interface';
--- packages/shared/ui/src/fragments/SSO/interface.ts
+++ packages/shared/ui/src/fragments/SSO/interface.ts
@@ -1,2 +1,3 @@
 export * as SIGNIN from './Signin/interface';
 export * as SIGNUP from './Signup/interface';
+export * as FORGOT from './Forgot/interface';
