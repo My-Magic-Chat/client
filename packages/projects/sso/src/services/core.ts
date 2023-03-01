198c85aed76759e9542d559b6edb25e5cb2d1661
leonardo.goncalves <leogoncalves.contato@gmail.com>
2023-03-01 13:16:16 -0300

    👽️ set firebase external authentication

--- packages/projects/sso/src/services/core.ts
+++ packages/projects/sso/src/services/core.ts
@@ -1,6 +1,10 @@
+import * as firebase from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
 import { Base, Cookies, Handler, Local, Session, Http } from '@client/services';
 import { Environment } from '@client/setup';
 import { SSO } from '@client/business';
+
 
 class Core {
     public http = Http;
@@ -16,18 +20,7 @@ class Core {
     constructor() {
         this.setEnv();
         Base.data = { domain: Environment.domain };
-
-        SSO.setCredentials({
-            appId: process.env.REACT_APP_ID,
-            apiKey: process.env.REACT_APP_API_KEY,
-            projectId: process.env.REACT_APP_AUTH_DOMAIN,
-            authDomain: process.env.REACT_APP_PROJECT_ID,
-            storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-            measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-            messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-        });
-
-        this.sso = new SSO();
+        this.startSSO();
     }
 
     private setEnv() {
@@ -36,6 +29,20 @@ class Core {
             domain: process.env.REACT_APP_DOMAIN,
             baseUrl: process.env.REACT_APP_BASE_URL,
         });
+    }
+
+    private startSSO() {
+        const app = initializeApp({
+            appId: process.env.REACT_APP_ID,
+            apiKey: process.env.REACT_APP_API_KEY,
+            projectId: process.env.REACT_APP_PROJECT_ID,
+            authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+            storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+            measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+            messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        });
+
+        this.sso = new SSO(app);
     }
 }
 
