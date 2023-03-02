11f69951f68f88a98a6de2ca238d651f8271450e
leonardo.goncalves <leogoncalves.contato@gmail.com>
2023-03-02 10:23:41 -0300

    🎨 export Toast component and utils

--- packages/shared/ui/src/components/index.ts
+++ packages/shared/ui/src/components/index.ts
@@ -4,4 +4,5 @@ export { default as Link } from './Link';
 export { default as Input } from './Input';
 export { default as Button } from './Button';
 export { default as Wrapper } from './Wrapper';
+export { Toast, ToastProvider, useToast } from './Toast';
 export { default as Form, FormControl, FormGroup, Handle, Validator, useControlContext } from './Form';
