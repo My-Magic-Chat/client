4e689368cbaa09627af7f8872da2465755f4ca2b
leonardo.goncalves <leogoncalves.contato@gmail.com>
2023-03-07 18:24:24 -0300

    🚨 fix default layout

--- packages/projects/manager/src/layout/Default/Default.tsx
+++ packages/projects/manager/src/layout/Default/Default.tsx
@@ -1,14 +1,33 @@
 import { Fragment } from '@client/ui';
 
 import { IProps } from './';
+import { BUTTONS } from './const';
 
 const Default = ({ children }: IProps) => {
+    const sidenav: Fragment.SIDENAV.IProps = {
+        buttons: BUTTONS,
+        goToInitial() {
+            console.log('goToInitial');
+        },
+        goTo(path) {
+            console.log('handleClick', path);
+        },
+    };
+
+    const navbar: Fragment.NAVBAR.IProps = {
+        path: '',
+        user: {
+            name: 'leozin',
+            email: 'leo.contato@gmail.com',
+            image: ''
+        },
+        handleClick() {
+            console.log('handleClick');
+        },
+    };
+
     return (
-        <Fragment.BoardWrapper
-            goTo={() => console.log('goTo')}
-            goToInitial={() => console.log('goToInitial')}
-            handleClick={() => console.log('handleClick')}
-        >
+        <Fragment.BoardWrapper navbar={navbar} sidenav={sidenav}>
             {children}
         </Fragment.BoardWrapper>
     );
